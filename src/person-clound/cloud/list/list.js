d1f72608b99d282a97d00ac66d1bf3b8b8260792
sunheng <sunheng@tvmining.com>
2017-04-10 18:16:28 +0800

    route

--- src/person-clound/cloud/list/list.js
+++ src/person-clound/cloud/list/list.js
@@ -1,67 +1,68 @@
 import React from 'react'
+import {hashHistory} from 'react-router'
 import {Icon, Row, Col} from 'antd'
 import {getFileList} from '../../api.js'
 
 import './list.css';
 
 let FileItem = React.createClass({
-  render(){
-    const {name, ext, path, onFileClick} = this.props;
-    return (
-        <div className="item" onClick={onFileClick(path)}>
-          <Icon className="icon" type="folder-open"/>
-          <p>{name}</p>
-        </div>
-    )
-  }
+    render(){
+        const {name, ext, path} = this.props;
+        return (
+            <div className="item" onClick={this.onFileClick}>
+                <Icon className="icon" type="folder-open"/>
+                <p>{name}</p>
+            </div>
+        )
+    },
+    onFileClick: function() {
+        const {name, ext, path} = this.props;
+        hashHistory.push(path);
+    }
 });
 
 let CloudList = React.createClass({
-  getInitialState: function() {
-    return {
-      file: [],
-      path: [],
-    }
-  },
-  render (){
-    let that = this;
-    let file = this.state.file.map(function(item, index) {
+    getInitialState: function() {
+        return {
+            file: [],
+            path: [],
+        }
+    },
+    render (){
+        let that = this;
+        let file = this.state.file.map(function(item, index) {
+            return (
+                <FileItem
+                    key={item.name + '-' + index}
+                    name={item.name}
+                    path={item.path}
+                    ext={item.ext}
+                />
+            )
+        });
+        return <div className="listBox">{file}</div>
 
-      return (
-          <FileItem
-              key={item.name + '-' + index}
-              name={item.name}
-              path={item.path}
-              ext={item.ext}
-              onFileClick={that.onFileClick}
-          />
-      )
-    });
-    return <div className="listBox">{file}</div>
+    },
+    componentDidMount(){
+        /*const {params} = this.props;
+         const {splat} = params;*/
+        let pathname = this.props.location.pathname;
+        let path_ext = pathname.replace('/cloud/list', '/');
+        this.getFile(path_ext);
+    },
+    getFile(path){
+        let that = this;
+        getFileList(path, function(res) {
+            that.setState({
+                file: res.file,
+                path: res.path.split('/'),
+                loading: false
+            })
+        }, function(err) {
+            console.log('err', err)
+        })
+    },
 
-  },
-  componentDidMount(){
-    /*const {params} = this.props;
-     const {splat} = params;*/
-    let pathname = this.props.location.pathname;
-    let path_ext = pathname.replace('/cloud/list', '/');
-    this.getFile(path_ext);
-  },
-  getFile(path){
-    let that = this;
-    getFileList(path, function(res) {
-      that.setState({
-        file: res.file,
-        path: res.path.split('/'),
-        loading: false
-      })
-    }, function(err) {
-      console.log('err', err)
-    })
-  },
-  onFileClick: function(path) {
-    console.log(path);
-  }
 });
 
 export default CloudList
--- src/person-clound/index.js
+++ src/person-clound/index.js
@@ -11,115 +11,108 @@ const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
 
 var R = React.createClass({
-  render(){
-    return (
-        <Router history={hashHistory}>
-          {/*使用这种*号通配后下列路由就失效*/}
-          {/* <Route path='*' component={Mylayout}/>*/}
-          <Route path='/' component={Mylayout}>
-            <IndexRoute component={CloudList}/>
-            <Route path='/cloud/list' component={CloudList}/>
-            <Route path='/todo' component={Todo}/>
-          </Route>
-        </Router>
-    )
-  }
+    render(){
+        return (
+            <Router history={hashHistory}>
+                {/*使用这种*号通配后下列路由就失效*/}
+                {/* <Route path='*' component={Mylayout}/>*/}
+                <Route path='/' component={Mylayout}>
+                    <IndexRoute component={CloudList}/>
+                    <Route path='/cloud/list' component={CloudList}/>
+                    <Route path='/cloud/*' component={CloudList}/>
+                    <Route path='/todo' component={Todo}/>
+                </Route>
+            </Router>
+        )
+    }
 });
 
 var Mylayout = React.createClass({
-  getInitialState: function() {
-    return {
-      path: [''],
-      abc: 123123
-    }
-  },
-  render(){
-    return <Layout>
-      <Header className="header">
-        <div className="logo"/>
-        <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{lineHeight: '64px'}}
-        >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Layout>
-        <Sider width={200} style={{background: '#fff'}}>
-          <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{height: '100%'}}
-          >
-            <Menu.Item key="1">
-              <span><Icon type="cloud"/><Link to={'/cloud/list'}>云盘</Link></span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <span><Icon type="database"/><Link to={'/todo'}>todo</Link></span>
-            </Menu.Item>
-            {/*<SubMenu key="sub2" title={<span><Icon type="laptop"/>subnav 2</span>}>
-             <Menu.Item key="5">option5</Menu.Item>
-             <Menu.Item key="6">option6</Menu.Item>
-             <Menu.Item key="7">option7</Menu.Item>
-             <Menu.Item key="8">option8</Menu.Item>
-             </SubMenu>*/}
-            <SubMenu key="sub3" title={<span><Icon type="notification"/>subnav 3</span>}>
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout style={{padding: '0 24px 24px'}}>
-          <Nav path={this.state.path}/>
-          <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 280}}>
-            {this.props.children}
-          </Content>
+    getInitialState: function() {
+        return {
+            path: ['']
+        }
+    },
+    render(){
+        return <Layout>
+            <Header className="header">
+                <div className="logo"/>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{lineHeight: '64px'}}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Layout>
+                <Sider width={200} style={{background: '#fff'}}>
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{height: '100%'}}
+                    >
+                        <Menu.Item key="1">
+                            <span><Icon type="cloud"/><Link to={'/cloud/list'}>云盘</Link></span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <span><Icon type="database"/><Link to={'/todo'}>todo</Link></span>
+                        </Menu.Item>
+                        {/*<SubMenu key="sub2" title={<span><Icon type="laptop"/>subnav 2</span>}>
+                         <Menu.Item key="5">option5</Menu.Item>
+                         <Menu.Item key="6">option6</Menu.Item>
+                         <Menu.Item key="7">option7</Menu.Item>
+                         <Menu.Item key="8">option8</Menu.Item>
+                         </SubMenu>*/}
+                        <SubMenu key="sub3" title={<span><Icon type="notification"/>subnav 3</span>}>
+                            <Menu.Item key="9">option9</Menu.Item>
+                            <Menu.Item key="10">option10</Menu.Item>
+                            <Menu.Item key="11">option11</Menu.Item>
+                            <Menu.Item key="12">option12</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout style={{padding: '0 24px 24px'}}>
+                    <Nav path={this.state.path}/>
+                    <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 280}}>
+                        {this.props.children}
+                    </Content>
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
-    </Layout>
-  },
-  getFile(path){
-    let that = this;
-    that.setState({
-      loading: true
-    });
+    },
+    getFile(path){
+        let that = this;
+        that.setState({
+            loading: true
+        });
 
-    getFileList(path, function(res) {
-      that.setState({
-        file: res.file,
-        path: res.path.split('/'),
-        loading: false
-      })
-    }, function(err) {
-      console.log('err', err)
-    })
-  },
-  componentWillReceiveProps(nextProps){
-    const {params} = nextProps;
-    const {splat} = params;
-
-    let pathname = this.props.location.pathname;
-    let path_ext = pathname.replace('/cloud/list', '/');
-    this.getFile(path_ext);
-  },
-  // 已存在的组件接收到新参数时调用
-  componentWillReceiveProps(nextProps){
-    const {params} = nextProps;
-    const {splat} = params;
-    let pathname = this.props.location.pathname;
-    let path_ext = pathname.replace('/cloud/list', '/');
-    this.getFile(path_ext);
-  },
-  changeRoute: function() {
-    alert(arguments);
-  }
+        getFileList(path, function(res) {
+            that.setState({
+                file: res.file,
+                path: res.path.split('/'),
+                loading: false
+            })
+        }, function(err) {
+            console.log('err', err)
+        })
+    },
+    // 已存在的组件接收到新参数时调用
+    componentWillReceiveProps(nextProps){
+        alert(1);
+        const {params} = nextProps;
+        const {splat} = params;
+        let pathname = this.props.location.pathname;
+        let path_ext = pathname.replace('/cloud/list', '/');
+        this.getFile(path_ext);
+    },
+    changeRoute: function() {
+        alert(arguments);
+    }
 });
 
 export default R
